cdfbb30b287d5128eac543606bf7cb5ae8e7ef2d
zking2000 <3065482@qq.com>
2023-10-15 02:45:19 -0500

    Create pressure.sh

---
+++ kcp_kdp/pressure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 定义一个函数来执行curl请求
+request_url() {
+    count=0
+    while [ $count -lt 100 ]; do
+        response_code=$(curl -o /dev/null -s -w "%{http_code}" http://52.190.26.254/httpd)
+        echo "HTTP Response Code: $response_code"
+        count=$((count+1))
+    done
+}
+
+# 定义要启动的线程数
+THREAD_COUNT=100
+
+# 启动多个线程
+for i in $(seq 1 $THREAD_COUNT); do
+    request_url &
+done
+
+# 等待所有后台进程完成
+wait
